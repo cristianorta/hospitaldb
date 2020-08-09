6fa2ccd94401ea0a21291d1dcdb6d2a1a2328d13
Cristian <cristian@bhinet.com.ar>
2020-08-09 09:02:07 -0300

    CORS y Express - primer commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const { dbConnection } = require('./database/config');
+
+// Crear el servidor express
+const app = express();
+
+// Configurar CORS
+app.use cors() );
+
+//Conexion a base de datos
+dbConnection();
+
+//Rutas
+app.get( '/', (req, res) => {
+
+    res.json({
+        ok: true,
+        msg: 'Hola Mundo'
+    })
+});
+
+app.listen( process.env.PORT, () => {
+    console.log('Servidor corriendo en puerto '+ process.env.PORT );
+});
